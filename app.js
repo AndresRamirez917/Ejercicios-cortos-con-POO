04a298e5fd44c75aa60ac042785d44bcc4cc6ec9
Andrés Ramirez <axy09179@gmail.com>
2023-01-11 00:01:45 -0500

    ejercicios

--- app.js
+++ app.js
@@ -13,6 +13,7 @@
 // console.log(user.fullname())
 
 
+
 // EJEMPLO 2 USANDO THIS. PARA LLAMAR LAS PROPIEDADES DEL OBJETO DENTRO DE UN MÉTODO
 // const user = {
 //     name : "andres",
@@ -41,30 +42,115 @@
 // account.witdraw()
 // console.log("Saldo después del retiro " + account.amount)
 
+
+
+
 // EJEMPLO 3 PASANDO LA CANTIDAD A DEPOSITAR Y RETIRAR A TRAVÉS DE SUS MÉTODOS
-const user = {
-    name : "andres",
-    lastname : "ramirez",
-    age : 43,
-    showName(){
-        return "Nombre " + this.name + " Apellido " + this.lastname + " Edad " + this.age
+// const user = {
+//     name : "andres",
+//     lastname : "ramirez",
+//     age : 43,
+//     showName(){
+//         return "Nombre " + this.name + " Apellido " + this.lastname + " Edad " + this.age
+//     }
+// }
+
+// console.log(user.showName())
+
+// const account = {
+//     number : "123412341234123412",
+//     amount : 100,
+//     deposit(quantity){
+//        this.amount = this.amount + quantity
+//     }, 
+//     witdraw(quantity){
+//         this.amount = this.amount - quantity
+//     }
+// }
+
+// account.deposit(300)
+// console.log("Saldo después del depósito " + account.amount)
+// account.witdraw(50)
+// console.log("Saldo después del retiro " + account.amount)
+
+
+
+
+// EJEMPLO 4 CONSTRUCTOR "GENERADOR DE OBJETOS"
+// const user = {
+//     name : "andres",
+//     lastname : "ramirez",
+//     age : 43,
+//     showName(){
+//         return "Nombre " + this.name + " Apellido " + this.lastname + " Edad " + this.age
+//         // O return `${this.name} ${this.lastname}`
+//     }
+// }
+
+// function Person() {
+//         this.name = "",
+//         this.lastname = "", 
+//         this.age = 0,
+//         // LA FUNCIÓN SHOWNAME OBLIGATORIAMENTE SE DEBE DECLARAR DE ESTA MANERA
+//         this.showName = function(){
+//             return `Nombre: ${this.name} Apellido: ${this.lastname} Edad: ${this.age}`
+//         }
+//     }
+
+// const user2 = new Person();
+// user2.name = "Jose"
+// user2.lastname = "Repelin"
+// user2.age = 50
+// console.log(user2.showName())
+    
+
+// EJEMPLO 5 USO DE LA PALABRA NEW
+// function Person(){
+//     // CON LA LÍNEA "USE ESTRICT" EVITO QUE LAS PROPIEDADES DE ESTE OBJETO SE LE ASIGNEN A UN OBJETO 
+//     // QUE ESTE EN UN NIVEL JERARQUICO MAYOR AL DE PERSON, (EN ESTE CASO SE LE ASIGNARIAN
+//     // ESAS PROPIEDADES AL OBJETO WINDOWS) EN CASO TAL DE QUE LA INSTANCIA DE UN NUEVO
+//     // OBJETO PERSON NO SE HAGA CON LA PALABRA NEW
+//     "use estrict"
+//     this.name = "",
+//     this.lastname = ""
+// }
+
+// // SI OMITO LA PALABRA NEW, LAS PROPIEDADES SE SE ASIGNARAN AL OBJETO WINDOWS EN ESTE CASO EN PARTICULAR
+// const person = new Person()
+// person.name = "Diego"
+// console.log(person.name)
+
+
+
+
+// EJEMPLO 6 PROTOTYPE
+function Person(name, lastname){
+    this.name = name,
+    this.lastname = lastname,
+    this.displayName = function(){
+        return `${this.name} ${this.lastname}`
     }
 }
 
-console.log(user.showName())
+const luigi = new Person("Luigi", "Bros");
+// CAMBIO DINAMICAMENTE DE PROPIEDADES EN OBJETOS
+luigi.lastname = "Bronx"
+const mario = new Person("Mario", "Bros");
+console.log(luigi.displayName());
+console.log(mario.displayName())
 
-const account = {
-    number : "123412341234123412",
-    amount : 100,
-    deposit(quantity){
-       this.amount = this.amount + quantity
-    }, 
-    witdraw(quantity){
-        this.amount = this.amount - quantity
-    }
+// CAMBIO DINAMICAMENTE DE FUNCIONES EN OBJETOS
+luigi.greet = function(){
+    console.log(`Hola ${this.name}, este método solo lo puedes usar tú`)
 }
 
-account.deposit(300)
-console.log("Saldo después del depósito " + account.amount)
-account.witdraw(50)
-console.log("Saldo después del retiro " + account.amount)
+luigi.greet()
+
+// SI QUIERO USAR EL MÉTODO GREET PARA MARIO PUEDO AGREGAR EL MÉTODO EN LA FUNCIÓN PERSON
+// PERO LO MAS RECOMENDADO ES HACERLO CON LA PROPIEDAD PROTOTYPE DE LA FUNCIÓN PERSON
+// QUE ALTERA EL CONSTRUCTOR AGREGANDO FUNCIONALIDADES QUE NOSOTROS NECESITEMOS
+ Person.prototype.greet = function(){
+    return `Ahora gracias a prototype ${this.name} puede usar también el método greet`
+ }
+
+ console.log(mario.greet())
